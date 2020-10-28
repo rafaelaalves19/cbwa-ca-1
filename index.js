6154ae1842cf7d379dc58feae7566d8adfe33770
Rafaela Alves <rafaelaalves.rla@gmail.com>
2020-10-28 16:49:15 +0000

    fixing2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const hostname = '0.0.0.0';
 
 app.get('/', (req, res) => {
